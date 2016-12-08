8aded567bafeea80500fa89fe6aebf48b4abafcd
Majeed Thaika <majeed.thaika@hotmail.com>
2016-12-08 21:39:47 +0700

    Some changes to gps simulator

--- src/main/scala/GPS_Simulator.scala
+++ src/main/scala/GPS_Simulator.scala
@@ -50,7 +50,7 @@ class TimeActor extends Actor{
   }
 }
 
-class ttetRawData(rawString: String) {
+class ttetRawData(rawString: String)  {
 
   val splitted = rawString.split(",")
   def imei: String = splitted(1)
