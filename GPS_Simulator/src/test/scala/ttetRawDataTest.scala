59ddeb920a18c20fa9ed0197bf3c85db894e71e4
Majeed Thaika <majeed.thaika@hotmail.com>
2016-12-12 14:48:16 +0700

    Cleaned up all directories

---
+++ GPS_Simulator/src/test/scala/ttetRawDataTest.scala
@@ -0,0 +1,17 @@
+// import org.scalatest.{FlatSpec, Matchers}
+
+// class ttetRawDataTest extends FlatSpec
+//   with Matchers{
+
+//   it should "convert raw string into fields" in {
+//     val record = new ttetRawData("1,tq000149,0.00000,0.00000,0.0,0.0,0.0,1,1,1433091600,30")
+//     record.imei shouldEqual "tq000149"
+//     record.lat shouldEqual 0.00000
+//     record.lng shouldEqual 0.00000
+//     record.speed shouldEqual 0.0f
+//     record.direction shouldEqual 0.0f
+
+//     record.datetime.toString shouldEqual "Mon Jun 01 00:00:00 ICT 2015"
+//   }
+
+// }
